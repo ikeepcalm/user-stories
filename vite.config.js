362898ab38bdaa31e41327ed2496ea9c258bf371
ikeepcalm <horokhbohdandmytrovich@gmail.com>
2025-09-16 17:04:45 +0300

    Fix deployment with Vite on Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/user-stories/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html')
+      }
+    }
   }
 })
